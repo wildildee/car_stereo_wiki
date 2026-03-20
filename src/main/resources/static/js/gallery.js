ba5527a8d0f2d8ea47639a1324c861dca481c65e
wildildee <you@example.com>
2026-03-20 13:42:43 -0400

    Added modal functionality for full-size image viewing in the gallery, with behavior updates in JS and CSS adjustments

--- src/main/resources/static/js/gallery.js
+++ src/main/resources/static/js/gallery.js
@@ -5,4 +5,16 @@ $(() => {
         $(".gallery-thumbnail").removeClass("is-focused");
         $(e.target).addClass("is-focused");
     });
+
+    $(".gallery-big-image").on("click", () => {
+        const src = $(".gallery-big-image").attr("src");
+        $("#modal-image").attr("src", src);
+        $("#image-modal").addClass("is-active");
+        $("html").addClass("is-clipped");
+    });
+
+    $(".modal-background, .modal-close").on("click", () => {
+        $("#image-modal").removeClass("is-active");
+        $("html").removeClass("is-clipped");
+    });
 })
